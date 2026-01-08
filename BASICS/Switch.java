8d41e01b549f3adf8eedaa3f9459c24242a8bbcc
reddy887 <avulareddybabu@gmail.com>
2026-01-08 16:28:35 +0530

    switch usin wrapper method

--- BASICS/Switch.java
+++ BASICS/Switch.java
@@ -1,3 +1,5 @@
+package BASICS;
+
 import java.util.Scanner;
 
 public class Switch {
---
+++ BASICS/switchwrapper.java
@@ -0,0 +1,19 @@
+package BASICS;
+
+public class switchwrapper {
+    	public static void main(String args[]) {
+	  int num=0;
+	  switch(Integer.compare(num,0)) {
+	  case -1:
+		  System.out.println("neagtive ");
+		  break;
+	  case 1:
+		  System.out.println("positive");
+		  break;
+	  case 0:
+		  System.out.println("zero");
+	  }
+	}
+
+    
+}
