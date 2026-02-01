3101a906efefd89bf0d81c42ba87441343d7eab7
reddy887 <avulareddybabu@gmail.com>
2026-02-01 05:51:39 +0530

    method of a particular object

--- methodreference/methodreferencepractice.java
+++ methodreference/methodreferencepractice.java
@@ -3,7 +3,7 @@ import java.util.*;
 // import java.util.function.Consumer;
 
 public class methodreferencepractice {
-    public static void greet(String names){
+    public void greet(String names){
         System.out.println("hey! hi how are you  "+names);
 
     }
@@ -11,7 +11,11 @@ public class methodreferencepractice {
     public static void main(String[] args) {
         List<String>names=Arrays.asList("reddy","babu","hari");
         // names.forEach(name->greet(name)
-        names.forEach(methodreferencepractice::greet);
+
+        //reference to an instance method of a particular object
+        methodreferencepractice mrf=new methodreferencepractice();
+        names.forEach(mrf::greet);
+        // names.forEach(methodreferencepractice::greet);
         
         // for(int i=0;i<names.size();i++){
         //     System.out.println(names.get(i));
